2c2543fe69f5b35f3df7e64151c8bf12e93f0c9c
ritusu <ritusu@cybage.com>
2019-06-14 13:48:32 +0530

    committing first

---
+++ rs-maven/src/main/java/rs_maven/rs_maven/add_num.java
@@ -0,0 +1,22 @@
+package rs_maven.rs_maven;
+
+/*public static void main(String args[]) 
+{
+    int num1=5;int num2=10; int sum;
+    sum = num1 + num2;
+
+    System.out.println("Sum of these numbers: "+sum);
+ }*/
+public class add_num{
+	
+	public static void main(String args[]){
+		
+		int no1 = 10, no2 = 5, sum;
+		sum = no1+no2;
+		System.out.println("Sum is:"+sum);
+	}
+	
+	
+}
+
+
